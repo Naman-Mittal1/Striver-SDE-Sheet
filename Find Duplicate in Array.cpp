2c56d8aeb38aca0717dadff3d2c05c7607750b31
Naman Mittal <mittalnaman08@gmail.com>
2023-06-06 21:50:05 +0530

    Sort 0 1 2

---
+++ Find Duplicate in Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	
+	sort(arr.begin(), arr.end());
+	int ans = -1;
+
+	for(int i=0; i<n; i++)
+	{
+		if(arr[i] == arr[i+1])
+		{
+			ans = arr[i];
+			return ans;
+		}
+	}
+
+	return ans;
+}
